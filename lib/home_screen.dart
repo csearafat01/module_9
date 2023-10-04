ad919bfe1ab0ec6dbbc8a66f1911608476db2f0e
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-10-05 01:59:02 +0600

    stateful Water Tracker app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,83 +1,102 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-/// Constructor
-/// CreateState
-/// InitState
-
-/// DidChangeDependencies - dependency change
-/// build - setState
-/// didUpdateWidget - Parent er configuration change
-
-/// deactive
-/// dispose
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return _HomeScreenState();
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int count = 0;
-
-  // 1
-  @override
-  void initState() {
-    print('init state');
-    /// Task when screen start
-    super.initState();
-  }
-
-  // 2
-  @override
-  void didChangeDependencies() {
-    print('Did change dependency');
-    super.didChangeDependencies();
-  }
-
-  @override
-  void didUpdateWidget(covariant HomeScreen oldWidget) {
-    print('did update widget');
-    super.didUpdateWidget(oldWidget);
-  }
+  final TextEditingController _amountOfGlassTEController = TextEditingController(text: '1');
+  List<WaterTrack> waterConsumeList = [];
+  int totalAmount = 0;
 
   @override
   Widget build(BuildContext context) {
-    print('Build method');
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Water tracker'),
       ),
-      body: Center(
-        child: Text('$count', style: const TextStyle(
-            fontSize: 32
-        ),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          // count = count + 1;
-          count++;
-          setState(() {}); // rebuild
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Total consume',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(
+                  '$totalAmount',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 50,
+                      child: TextField(
+                        controller: _amountOfGlassTEController,
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        int amount = int.tryParse(_amountOfGlassTEController.text.trim()) ?? 1;
+                        totalAmount += amount;
+                        WaterTrack waterTrack = WaterTrack(DateTime.now(), amount);
+                        waterConsumeList.add(waterTrack);
+                        setState(() {});
+                      },
+                      child: const Text('Add'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: waterConsumeList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 4,
+                    child: ListTile(
+                      onLongPress: () {
+                        if (kDebugMode) {
+                          print('on long press');
+                        }
+                        // TODO : Delete the item from list
+                        // TODO : totalAmount theke remove korte hobe
+                      },
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      title: Text(DateFormat('HH:mm:ss  dd-MM-yyyy')
+                          .format(waterConsumeList[index].time)),
+                      trailing: Text(
+                        '${waterConsumeList[index].noOfGlass}',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ),
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
+}
 
-  // 5
-  @override
-  void deactivate() {
-    print('deactived');
-    super.deactivate();
-  }
+class WaterTrack {
+  final DateTime time;
+  final int noOfGlass;
 
-  // 6
-  @override
-  void dispose() {
-    print('dispose');
-    super.dispose();
-  }
+  WaterTrack(this.time, this.noOfGlass);
 }
