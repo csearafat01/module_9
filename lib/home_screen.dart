ec1dd55ce4bfe483375c08e62e4bad69addc5800
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-10-05 01:51:02 +0600

    stateful Water Tracker app class task

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,83 +1,130 @@
 import 'package:flutter/material.dart';
-
-/// Constructor
-/// CreateState
-/// InitState
-
-/// DidChangeDependencies - dependency change
-/// build - setState
-/// didUpdateWidget - Parent er configuration change
-
-/// deactive
-/// dispose
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _HomeScreenState();
-  }
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int count = 0;
+  final TextEditingController _amountOfGlassTEController =
+      TextEditingController(text: '1');
+  List<WaterTrack> waterConsumeList = [];
+  int totalAmount = 0;
 
-  // 1
   @override
-  void initState() {
-    print('init state');
-    /// Task when screen start
-    super.initState();
+  void dispose() {
+    _amountOfGlassTEController.dispose();
+    super.dispose();
   }
 
-  // 2
-  @override
-  void didChangeDependencies() {
-    print('Did change dependency');
-    super.didChangeDependencies();
+  void addWaterTrack() {
+    final input = _amountOfGlassTEController.text.trim();
+    if (input.isNotEmpty) {
+      final amount = int.tryParse(input);
+      if (amount != null) {
+        totalAmount += amount;
+        final waterTrack = WaterTrack(DateTime.now(), amount);
+        waterConsumeList.add(waterTrack);
+        _amountOfGlassTEController.clear();
+        setState(() {});
+      } else {
+        // Show an error message to the user
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Invalid input. Please enter a valid number.'),
+          ),
+        );
+      }
+    }
   }
 
-  @override
-  void didUpdateWidget(covariant HomeScreen oldWidget) {
-    print('did update widget');
-    super.didUpdateWidget(oldWidget);
+  void deleteWaterTrack(int index) {
+    if (index >= 0 && index < waterConsumeList.length) {
+      final deletedAmount = waterConsumeList[index].noOfGlass;
+      totalAmount -= deletedAmount;
+      waterConsumeList.removeAt(index);
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    print('Build method');
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Water tracker'),
       ),
-      body: Center(
-        child: Text('$count', style: const TextStyle(
-            fontSize: 32
-        ),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          // count = count + 1;
-          count++;
-          setState(() {}); // rebuild
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Total consume',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Text(
+                  '$totalAmount',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 50,
+                      child: TextField(
+                        controller: _amountOfGlassTEController,
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: addWaterTrack,
+                      child: const Text('Add'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: waterConsumeList.length,
+              itemBuilder: (context, index) {
+                final waterTrack = waterConsumeList[index];
+                return Card(
+                  elevation: 4,
+                  child: ListTile(
+                    onLongPress: () {
+                      deleteWaterTrack(index);
+                    },
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                    title: Text(DateFormat('HH:mm:ss a dd-MM-yyyy')
+                        .format(waterTrack.time)),
+                    trailing: Text(
+                      '${waterTrack.noOfGlass}',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
+}
 
-  // 5
-  @override
-  void deactivate() {
-    print('deactived');
-    super.deactivate();
-  }
+class WaterTrack {
+  final DateTime time;
+  final int noOfGlass;
 
-  // 6
-  @override
-  void dispose() {
-    print('dispose');
-    super.dispose();
-  }
+  WaterTrack(this.time, this.noOfGlass);
 }
